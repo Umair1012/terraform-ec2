4aa5eaf23ee036194336b551162f58f013fe794a
Umair1012 <139431715+Umair1012@users.noreply.github.com>
2025-11-18 12:26:06 +0000

    Files added

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -19,56 +19,22 @@ file_permission = "0400"  # Restrict access to the private key
 
 
 #Call EC2 module
-module "frontend_ec2" {
+module "dev_ec2" {
   source = "../../modules/ec2"
   environment = "dev"
-  ami = "ami-0ecb62995f68bb549"
-  instance_count = 1
+  ami = "ami-0cae6d6fe6048ca2c"
+  instance_count = 2
   ec2_username = "ubuntu"
   key_name = aws_key_pair.default.key_name
-  ec2_sg = "frontend-${var.environment}"
-  ssh_port = 22
+  ec2_sg = "dev-${var.environment}"
+  ssh_port = 80
   tags = {
-    Name = "Frontend-${var.environment}"
+    Name = "Dev-${var.environment}"
     Environment = "dev"
   }
   volume_size = 10
   volume_type = "gp3"
   instance_type = "t2.micro"
+  user_data = "file('${path.module}/scripts/install_services.sh')"
 } 
 
-module "backend_ec2" {
-  source = "../../modules/ec2"
-  environment = "dev"
-  ami = "ami-0ecb62995f68bb549"
-  instance_count = 1
-  ec2_username = "ubuntu"
-  key_name = aws_key_pair.default.key_name
-  ec2_sg = "backend-${var.environment}"
-  ssh_port = 22
-  tags = {
-    Name = "Backend-${var.environment}"
-    Environment = "dev"
-  }
-  volume_size = 15
-  volume_type = "gp3"
-  instance_type = "t2.micro"
-} 
-
-module "database_ec2" {
-  source = "../../modules/ec2"
-  environment = "dev"
-  ami = "ami-0ecb62995f68bb549"
-  instance_count = 2
-  ec2_username = "ubuntu"
-  key_name = aws_key_pair.default.key_name
-  ec2_sg = "database-${var.environment}"
-  ssh_port = 22
-  tags = {
-    Name = "Database-${var.environment}"
-    Environment = "dev"
-  }
-  volume_size = 20
-  volume_type = "gp3"
-  instance_type = "t2.micro"
-} 
